0a6f2375f57a7b68172c81c039c64939ce0b6529
Rohitkuru <rohit.kuru@gmail.com>
2020-05-30 23:05:36 +0530

    removed DB create

--- app.py
+++ app.py
@@ -33,12 +33,12 @@ login_manager.init_app(app)
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-
+'''
 with app.app_context():
     db.create_all()
 
 
-'''
+
 if __name__ == '__main__':
  
     app.run()
