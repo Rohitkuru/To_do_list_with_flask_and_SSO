cfe9404470734462cd14055a134db589204ece29
Rohitkuru <rohit.kuru@gmail.com>
2020-05-30 22:19:08 +0530

    removed extra space from app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_login import LoginManager
 from auth.auth_views import auth
 from operation.operation_views import operation
-
 from reports_and_download.reports_and_download import reports_and_download
 from models import *
 from flask_dance.contrib.github import make_github_blueprint
@@ -11,9 +10,7 @@ import config
 import os
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
-
 app = Flask(__name__)
-
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///to_do_list.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -21,7 +18,6 @@ app.secret_key = 'notmuchsecreat'
 app.config.from_pyfile("config.py")
 
 db.init_app(app)
-
 
 github_blueprint = make_github_blueprint(client_id=config.client_id,client_secret=config.client_secret)
 app.register_blueprint(github_blueprint,url_prefix="/github_login")
