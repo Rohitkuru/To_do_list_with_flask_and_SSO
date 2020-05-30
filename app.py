f358548a1a429f86d3af0a425fadc0d12e1843a1
Rohitkuru <rohit.kuru@gmail.com>
2020-05-30 21:51:44 +0530

    Removed Ngrok settings which were added earlier

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_login import LoginManager
 from auth.auth_views import auth
 from operation.operation_views import operation
-from flask_ngrok import run_with_ngrok
+
 from reports_and_download.reports_and_download import reports_and_download
 from models import *
 from flask_dance.contrib.github import make_github_blueprint
@@ -13,7 +13,6 @@ os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
 
 app = Flask(__name__)
-run_with_ngrok(app)
 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///to_do_list.sqlite3'
@@ -39,7 +38,6 @@ login_manager.init_app(app)
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-
 
 
 if __name__ == '__main__':
